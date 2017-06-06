8661d4cedfc1f35e7f67163c063942b70d808051
jlinskeseder <jakob.linskeseder@karriere.at>
2017-06-06 16:24:38 +0200

    Inject whole config object instead of single value

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-include __DIR__ . "/../vendor/autoload.php";
+include __DIR__ . '/../vendor/autoload.php';
 
 $config = new \Karriere\Mocky\Configuration(__DIR__ . '/../config/mocky.php');
 
-$mocky = new \Karriere\Mocky\Mocky($config['spec-dir']);
+$mocky = new \Karriere\Mocky\Mocky($config);
 
 $mocky->run();
